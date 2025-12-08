9543b24127dbcb27e43af32c56c61cdf185d6f4a
Niklas Ekman <nikl.ekman@gmail.com>
2025-12-08 13:02:25 +0100

    Add object remove empty

---
+++ src/object.test.ts
@@ -0,0 +1,21 @@
+import { queryParamsCreate } from "./query-params";
+import {objectRemoveEmpty} from "./object";
+
+describe("object", () => {
+  it("filter out non-empty keys", () => {
+    const data = {
+			foo: undefined,
+			bar: "",
+			hello: "world",
+			x: {
+				a: 1,
+				b: 0,
+			}
+		};
+
+    expect(objectRemoveEmpty(data)).toEqual({
+			hello: "world",
+			x: { a: 1 },
+		});
+  });
+});
