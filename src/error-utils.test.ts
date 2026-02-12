bc9b5ff1c58dedccdd450b93ebaaae83ced9b8c2
Niklas Ekman <nikl.ekman@gmail.com>
2026-02-12 20:24:42 +0100

    Be able to detect errors

---
+++ src/error-utils.test.ts
@@ -0,0 +1,11 @@
+import {errorDetect} from "./errors-utils";
+
+describe('error-utils', () => {
+	test.each([
+		[{ message: "foo" }, true],
+		[new Error('foo'), true],
+		["x", false]
+	])('can detect errors', (input, expected) => {
+		expect(errorDetect(input)).toBe(expected);
+	})
+})
