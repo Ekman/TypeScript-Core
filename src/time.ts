5d6d57e408c6a258e28fc30aa5306d2bee345af7
Niklas Ekman <nekm@netlight.com>
2026-04-15 09:02:37 +0200

    Add interval

--- src/time.ts
+++ src/time.ts
@@ -1,3 +1,8 @@
 export function timer(waitMs: number): Promise<void> {
 	return new Promise((resolve) => setTimeout(resolve, waitMs));
 }
+
+export function interval(waitMs: number, fn: () => void | Promise<void>): () => void {
+	const intervalId = setInterval(fn, waitMs);
+	return () => clearInterval(intervalId);
+}
