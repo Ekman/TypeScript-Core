d34c67f4f35ee08379739b9d082ce4847d1dac4f
Niklas Ekman <nekm@netlight.com>
2026-04-15 14:49:00 +0200

    Format

--- src/enum.ts
+++ src/enum.ts
@@ -1,11 +1,16 @@
 export class EnumParseError extends Error {}
 
-export function enumParse<T extends Record<string, string>>(enumObj: T, value: string): T[keyof T] {
-	const match = Object.values(enumObj).find(v => v === value);
+export function enumParse<T extends Record<string, string>>(
+	enumObj: T,
+	value: string,
+): T[keyof T] {
+	const match = Object.values(enumObj).find((v) => v === value);
 
 	if (!match) {
 		const enumValues = Object.values(enumObj).join(", ");
-		throw new EnumParseError(`"${value}" is not a valid enum value. Expected one of: ${enumValues}`);
+		throw new EnumParseError(
+			`"${value}" is not a valid enum value. Expected one of: ${enumValues}`,
+		);
 	}
 
 	return match as T[keyof T];
--- src/time.ts
+++ src/time.ts
@@ -2,7 +2,10 @@ export function timer(waitMs: number): Promise<void> {
 	return new Promise((resolve) => setTimeout(resolve, waitMs));
 }
 
-export function interval(waitMs: number, fn: () => void | Promise<void>): () => void {
+export function interval(
+	waitMs: number,
+	fn: () => void | Promise<void>,
+): () => void {
 	const intervalId = setInterval(fn, waitMs);
 	return () => clearInterval(intervalId);
 }
